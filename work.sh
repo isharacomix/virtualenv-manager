0da3438032b27bf0f171925425b048b0c368f5b3
Barry Peddycord III <ishara@isharacomix.org>
2015-02-26 11:57:54 -0500

    Update work.sh

--- work.sh
+++ work.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 cd $1
-/bin/bash -c ". ../.virtualenvs/$1/bin/activate; export PS1='('$1')\w$ '; exec /bin/bash -i"
+/bin/bash -c ". ../.virtualenvs/$1/bin/activate; export PS1='\[\e]0;[$1]\w\007\]('$1')\w\[\e[1;31m\]\$(__git_ps1)\[\e[0m\]$ '; exec /bin/bash --rcfile '../.git-prompt.sh' -i"
 
 echo "done"
